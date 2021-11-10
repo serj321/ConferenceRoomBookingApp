71064b18b8573a561ecbf17f16925395ac59dbba
VisheshMendiratta <65038166+VisheshMendiratta@users.noreply.github.com>
2021-11-09 19:48:47 -0500

    Update ConferenceRoomData

--- com/mydomain/mainpackage/data/ConferenceRoomData.java
+++ com/mydomain/mainpackage/data/ConferenceRoomData.java
@@ -12,6 +12,17 @@ package com.mydomain.mainpackage.data;
 	}
 	public List<ConferenceRoom> findAvailableConferenceRooms(Date start, int duration, Integer minimumCapacity, BigDecimal maximumRate) throws SQLException
 	{
+		ConferenceRoomDAOFactory roomDAO = new ConferenceRoomDAOFactory();
+		List<ConferenceRoom> rooms = new ArrayList<ConferenceRoom>();
+		for(int i = 0; i < 2; i++)
+		{
+			rooms.add(getConferenceRoomDAO());
+		}
+	}
+	
+	public ConferenceRoom getConferenceRoom(String roomCode) throws SQLException
+	{
 		
 	}
+	
 }

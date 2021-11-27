d4c8d03bbe2f64a40e4004d7cada770aae141baf
serj321 <77907685+serj321@users.noreply.github.com>
2021-11-27 10:39:13 -0500

    double checked stage 2

--- src/com/mydomain/mainpackage/data/ConferenceRoomBookingData.java
+++ src/com/mydomain/mainpackage/data/ConferenceRoomBookingData.java
@@ -2,6 +2,9 @@ package com.mydomain.mainpackage.data;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -12,7 +15,7 @@ import ca.senecacollege.prg556.hocorba.bean.ConferenceRoomBooking;
 
 public class ConferenceRoomBookingData {
 	
-	public ConferenceRoomBooking bookConferenceRoom(int clientId, String roomCode, Date start, int duration){
+	public ConferenceRoomBooking bookConferenceRoom(int clientId, String roomCode, Date start, int duration) throws ServletException{
 		try{
 			ConferenceRoomData crData = new ConferenceRoomData();
 			ConferenceRoom cr = crData.getConferenceRoom(roomCode);
@@ -24,30 +27,27 @@ public class ConferenceRoomBookingData {
 		}catch(SQLException sqle){
 			throw new ServletException(sqle);
 		}
+		return null;
 	}
 	
-	public void cancelConferenceRoomBooking(int bookingCode){
-		try{
-			
-		}catch(SQLException sqle){
-			throw new ServletException(sqle);
-		}
+	public void cancelConferenceRoomBooking(int bookingCode) throws ServletException{
 	}
 	
-	public ConferenceRoomBooking getConferenceRoomBooking(int bookingCode){
-		try{
-			//NEED TO READ FURTHER
-			
-			
-		}catch(SQLException sqle){
-			throw new ServletException(sqle);
-		}
+	public ConferenceRoomBooking getConferenceRoomBooking(int bookingCode) throws ServletException{
+		return null;
 	}
 	
-	public List<ConferenceRoomBooking> getConferenceRoomBookings (int clientId){
+	public List<ConferenceRoomBooking> getConferenceRoomBookings (int clientId) throws ServletException, ParseException{
 		try{
-			List<ConferenceRoomBooking> crBookings = {new ConferenceRoom("111", "Room 2", 35, 150.00), 
-			                                          new ConferenceRoom("222","Room 1", 25, 100.00)};
+			ConferenceRoomData crData = new ConferenceRoomData();
+			ConferenceRoom cr1 = crData.getConferenceRoom("111");
+			ConferenceRoom cr2 = crData.getConferenceRoom("222");
+			SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd, yyyy");
+			Date date1 = formatter.parse("October 23, 2021");
+			Date date2 = formatter.parse("October 25, 2021");
+			List<ConferenceRoomBooking> crBookings = new ArrayList<ConferenceRoomBooking>();
+			crBookings.add(new ConferenceRoomBooking(1, cr1.getCode(), cr1.getName(), cr1.getCapacity(), date1, 90, BigDecimal.valueOf(150.00)));
+			crBookings.add(new ConferenceRoomBooking(2, cr2.getCode(), cr2.getName(), cr2.getCapacity(), date2, 120, BigDecimal.valueOf(100.00)));
 			return crBookings;
 		}catch(SQLException sqle){
 			throw new ServletException(sqle);
